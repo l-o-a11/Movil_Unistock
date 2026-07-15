6d6f40a71e0220435887f237edc31b4b7cba7619
Montoya-08 <gelicamontoyt@gmail.com>
2026-07-15 15:40:10 -0500

    Movil productos/fichatecnica

--- lib/domain/menu/menu_page.dart
+++ lib/domain/menu/menu_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../empleados/presentation/empleados_page.dart';
 import '../../shared/widgets/global_bottom_nav.dart';
-import '../categories/categories_page.dart';
+import '../product_categories/product_categories_page.dart';
 import '../compras/compras.dart';
 import '../insumos/insumos_page.dart';
 import '../proveedores/features/presentation/pages/proveedores_page.dart';
@@ -173,7 +173,7 @@ class MenuPage extends StatelessWidget {
                         onTap: () => Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (_) => const CategoriesPage(),
+                            builder: (_) => const ProductCategoriesPage(),
                           ),
                         ),
                       ),
